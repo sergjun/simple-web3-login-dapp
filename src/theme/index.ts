1c96be6662af3b66fb8834baba002ca18d05f663
sergjun <serrgio.madureira@gmail.com>
2021-09-30 20:55:18 -0300

    changed the main font of the project and the shadow outline to match the rest of the project

---
+++ src/theme/index.ts
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export default extendTheme({
+  fonts: {
+    heading: "Open Sans",
+    body: "Open Sans",
+  },
+
+  shadows: {
+    outline: "0 0 0 3px #C6F6D5",
+  },
+});
